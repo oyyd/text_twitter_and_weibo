ab5fb2dac788db93c200e7540ea7ed77d50f5d67
oyyd <oyydoibh@gmail.com>
2015-02-28 03:04:49 +0800

    add twitter login

--- twitter_weibo.js
+++ twitter_weibo.js
@@ -4,22 +4,29 @@ var childProcess = require('child_process');
 var phantomjsPath = require('phantomjs').path;
 var pagesScript = __dirname+'/lib/pages.js';
 
+var test_string = '测试测试';
+
 //get option file
 var option = JSON.parse(fs.readFileSync('option.json', 'utf8'));
 
-//spawn phantomjs
-var phantomjsArgs = [pagesScript, JSON.stringify(option.accounts)];
+//spawn phantomjs and send test string
+var phantomjsArgs = [
+  pagesScript, 
+  JSON.stringify(option.accounts),
+  test_string, 
+];
 
 var phantomjsProc = childProcess.spawn(phantomjsPath, phantomjsArgs);
 
-//handle messages
+var message = "";
+
+//handle message
 phantomjsProc.stdout.on('data', function(data){
+  message += data;
   console.log('stdout: '+data);
 });
 
 phantomjsProc.stdout.on('close', function(code){
+  fs.writeFileSync('result.txt', message);
   console.log('exit with: '+code);
 });
-
-// send test string
-var test_string = '测试测试';
